181916e96b2ad7d595831a43096dd627a8705932
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-10-17 20:05:45 +0200

    Swagger setup

--- src/main/java/com/example/isa/Application.java
+++ src/main/java/com/example/isa/Application.java
@@ -2,14 +2,15 @@ package com.example.isa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableWebMvc
-public class IsaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
