9305816576b09f05d1b6bac06cfa4e1588e54e64
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-10-31 11:34:30 +0100

    Added check if user exists

--- backend/src/main/java/com/example/isa/controller/UserController.java
+++ backend/src/main/java/com/example/isa/controller/UserController.java
@@ -4,10 +4,13 @@ import com.example.isa.dto.PatientDto;
 import com.example.isa.model.Address;
 import com.example.isa.model.Gender;
 import com.example.isa.model.Patient;
+import com.example.isa.model.User;
 import com.example.isa.service.interfaces.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,21 +34,27 @@ public class UserController {
 
     @PostMapping
     @ApiOperation(value = "Register a patient.", httpMethod = "POST")
-    public ResponseEntity<?> registerPatient(@RequestBody PatientDto dto) {
-        Gender gender = dto.getGender().trim().equalsIgnoreCase("female") ? Gender.FEMALE : Gender.MALE;
-        Patient patient = Patient.builder()
-                .personalId(dto.getPersonalId())
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .email(dto.getEmail())
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .phoneNumber(dto.getPhoneNumber())
-                .gender(gender)
-                .occupation(dto.getOccupation())
-                .address(new Address(dto.getStreet(), dto.getNumber(), dto.getCity(), dto.getCountry()))
-                .institutionInfo(dto.getInstitutionInfo())
-                .build();
-        service.register(patient);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Patient> registerPatient(@RequestBody PatientDto dto) {
+        UserDetails user = service.loadUserByUsername(dto.getEmail());
+        if(user != null) {
+            Gender gender = dto.getGender().trim().equalsIgnoreCase("female") ? Gender.FEMALE : Gender.MALE;
+            Patient patient = Patient.builder()
+                    .personalId(dto.getPersonalId())
+                    .firstName(dto.getFirstName())
+                    .lastName(dto.getLastName())
+                    .email(dto.getEmail())
+                    .password(passwordEncoder.encode(dto.getPassword()))
+                    .phoneNumber(dto.getPhoneNumber())
+                    .gender(gender)
+                    .occupation(dto.getOccupation())
+                    .address(new Address(dto.getStreet(), dto.getNumber(), dto.getCity(), dto.getCountry()))
+                    .institutionInfo(dto.getInstitutionInfo())
+                    .build();
+            service.register(patient);
+            return new ResponseEntity<>(patient, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        }
+
     }
 }
