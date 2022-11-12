d5bdac9f1f6d3392b4dee1e20c0fa2e970bad00f
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-11-12 21:48:38 +0100

    ModelMapper

--- backend/src/main/java/com/example/isa/dto/PatientDto.java
+++ backend/src/main/java/com/example/isa/dto/PatientDto.java
@@ -27,34 +27,5 @@ public class PatientDto {
     private String city;
     private String country;
     private String institution;
-
-    public PatientDto(Patient patient) {
-    	this.personalId = patient.getPersonalId();
-    	this.firstName = patient.getFirstName();
-    	this.lastName = patient.getLastName();
-    	this.email = patient.getEmail();
-    	this.password = patient.getPassword();
-    	this.phoneNumber = patient.getPhoneNumber();
-    	this.gender = patient.getGender().toString();
-    	this.occupation = patient.getOccupation();
-    	this.homeAddress = patient.getAddress().getStreet();
-    	this.city = patient.getAddress().getCountry();
-    	this.country = patient.getAddress().getCountry();
-    	this.institution = patient.getInstitution();
-    }
-    public Patient convert() {
-    	Patient retVal = new Patient();
-    	retVal.setPersonalId(this.getPersonalId());
-    	retVal.setFirstName(this.getFirstName());
-    	retVal.setLastName(this.getLastName());
-    	retVal.setEmail(this.getEmail());
-    	retVal.setPassword(this.getPassword());
-    	retVal.setPhoneNumber(this.getPhoneNumber());
-    	retVal.setGender(Gender.valueOf(this.getGender()));
-    	retVal.setOccupation(this.getOccupation());
-    	retVal.setAddress(new Address(this.getHomeAddress(), this.getCity(), this.getCountry()));
-    	retVal.setInstitution(this.getInstitution());
-    	return retVal;
-    }
 }
 
