212e1aea9868523df03bbca252c7a06fd04c318e
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-13 17:42:29 +0100

    Feature backend search BB by title, city

--- backend/src/main/java/com/example/isa/repository/BloodBankRepository.java
+++ backend/src/main/java/com/example/isa/repository/BloodBankRepository.java
@@ -1,12 +1,24 @@
 package com.example.isa.repository;
 
 import com.example.isa.model.BloodBank;
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BloodBankRepository extends JpaRepository<BloodBank, UUID> {
-
+	@Query("from BloodBank b where b.title like CONCAT('%', ?1, '%')")
+	public List<BloodBank> findByTitleLike(String title);
+	
+	@Query("from BloodBank b where b.address.city like CONCAT('%', ?1, '%')")
+	public List<BloodBank> findByCityLike(String city); 
+	
+	@Query("from BloodBank b where b.title like CONCAT('%', ?1, '%') and b.address.city like CONCAT('%', ?2, '%')")
+	public List<BloodBank> findByTitleAndCityLike(String title,String city);
 }
--- backend/src/main/java/com/example/isa/service/implementation/BloodBankServiceImpl.java
+++ backend/src/main/java/com/example/isa/service/implementation/BloodBankServiceImpl.java
@@ -25,7 +25,19 @@ public class BloodBankServiceImpl implements BloodBankService {
 
     @Override
     public List<BloodBank> search(Sort sort, List<String> searchCriteria) {
-        //Searching to be implemented by student 2.
-        return repository.findAll(sort);
+    	String titleString = searchCriteria.get(0);
+    	String cityString = searchCriteria.get(1);
+        if (titleString.equals("") && cityString.equals("")) {
+        	return repository.findAll();
+        }
+        else if(titleString.equals("")){
+        	return repository.findByCityLike(cityString);
+        }
+        else if(cityString.equals("")){
+        	return repository.findByTitleLike(titleString);
+        }
+        else {
+        	return repository.findByTitleAndCityLike(titleString, cityString);
+        }
     }
 }
