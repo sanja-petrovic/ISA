b886ad05220bea1b0c62034ed3537bbdcedf1da8
MMG-lmg <37846236+MMG-lmg@users.noreply.github.com>
2022-11-15 22:02:08 +0100

    Create BloodUsagePdfController.java
    Just a simple way of taking in PSW reports sent as pdf.
    Tested via postman seems ok.

---
+++ backend/src/main/java/com/example/isa/controller/BloodUsagePdfController.java
@@ -0,0 +1,37 @@
+package com.example.isa.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/bloodUsage" )
+public class BloodUsagePdfController {
+	public BloodUsagePdfController() {}
+	@PostMapping(path = "/upload")
+	public ResponseEntity<String> uploadToLocalFileSystem(@RequestParam("file") MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		Path path = Paths.get("BloodUsageReports" + File.separator + fileName);
+		//+
+		try {
+			Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return  ResponseEntity.ok(path.toString());
+	}
+
+}
