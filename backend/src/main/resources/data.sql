159be1d466ad69e0cda95ee936d9bdd3085c125f
Sanja Petrović <sanjica.petrovic7@gmail.com>
2022-12-22 10:37:09 +0100

    Improve styling

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -123,7 +123,7 @@ values (uuid_generate_v4(),'2023-02-02 9:30:00',15,'NOT_SCHEDULED','16e4a8c2-3e8
 insert into public.appointments(id,date_time,duration,status,blood_bank_id,blood_donor_id)
 values (uuid_generate_v4(),'2023-02-02 18:00:00',30,'NOT_SCHEDULED','16e4a8c2-3e86-4e93-825f-24e36cb29669',null);
 insert into public.appointments(id,date_time,duration,status,blood_bank_id,blood_donor_id)
-values (uuid_generate_v4(),'2023-02-02 18:00:00',30,'NOT_SCHEDULED','16e4a8c2-3e86-4e93-825f-24e36cb29669', null);
+values (uuid_generate_v4(),'2023-02-05 12:00:00',30,'NOT_SCHEDULED','16e4a8c2-3e86-4e93-825f-24e36cb29669', null);
 insert into public.appointments(id,date_time,duration,status,blood_bank_id,blood_donor_id)
 values (uuid_generate_v4(),'2022-12-30 18:00:00',30,'SCHEDULED','16e4a8c2-3e86-4e93-825f-24e36cb29655', '16e4a8c2-3e86-4e93-825f-24e36cb29645');
 insert into public.appointments(id,date_time,duration,status,blood_bank_id,blood_donor_id)
--- frontend/src/app/appointments/appointments.component.ts
+++ frontend/src/app/appointments/appointments.component.ts
@@ -3,27 +3,38 @@ import {AppointmentService} from "../services/AppointmentService";
 import {Appointment} from "../model/Appointment";
 import {ActivatedRoute, Router} from '@angular/router';
 import {DatePipe} from '@angular/common';
+import { BloodBank } from '../model/BloodBank';
+import { BloodBankService } from '../services/BloodBankService';
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.css', '../banks-page/banks-page.component.css']
 })
 export class AppointmentsComponent implements OnInit {
-  constructor(private appointmentService: AppointmentService, private route: ActivatedRoute, private router: Router, private datePipe: DatePipe) { }
+  constructor(private appointmentService: AppointmentService, private bloodBankService: BloodBankService, private route: ActivatedRoute, private router: Router, private datePipe: DatePipe) { }
 
   public appointments: Appointment[] = [];
+  public bloodBank: BloodBank;
 
   public id: string;
 
   getAppointments() {
     this.appointmentService.getAllAvailableByBloodBank(this.id).subscribe(data => {
       this.appointments = data;
+      console.log(data);
+    })
+  }
+
+  getBloodBank() {
+    this.bloodBankService.getAll().subscribe(data => {
+      this.bloodBank = data.filter(b => b.id === this.id).at(0);
     })
   }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this.getAppointments();
+    this.getBloodBank();
   }
 
   formatDate(appointment: Appointment): string {
@@ -37,7 +48,7 @@ export class AppointmentsComponent implements OnInit {
   schedule(appointment: Appointment): void {
     this.appointmentService.schedule(appointment.id).subscribe(res => {
       console.log(res);
-      this.router.navigate(["/appointments"]);
+      window.location.reload();
     },
     error => {
       alert(error.error.message);
