14c8a7388c693980250fe2ee89dd934cd8475cb1
Sanja Petrović <sanjica.petrovic7@gmail.com>
2023-01-22 20:56:55 +0100

    chore: init location simulator project

---
+++ location-simulator/src/main/java/com/example/locationsimulator/LocationSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationSimulatorApplication.class, args);
+	}
+
+}
---
+++ location-simulator/src/main/java/com/example/locationsimulator/model/Location.java
@@ -0,0 +1,13 @@
+package com.example.locationsimulator.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+    private double latitude;
+    private double longitude;
+}
