67679b281257bdeb50b8dd99cc8a8283a7d1aa06
Sanja Petrović <sanjica.petrovic7@gmail.com>
2023-01-26 15:47:59 +0100

    feat: messages to hospital

--- backend/src/main/java/com/example/isa/kafka/consumers/TrackingResponseConsumer.java
+++ backend/src/main/java/com/example/isa/kafka/consumers/TrackingResponseConsumer.java
@@ -1,8 +1,8 @@
 package com.example.isa.kafka.consumers;
 
-import com.example.isa.dto.locator.TrackingRequestDto;
+import com.example.isa.dto.BloodRequestResponseDto;
 import com.example.isa.dto.locator.TrackingResponseDto;
-import com.example.isa.model.locator.TrackingRequest;
+import com.example.isa.kafka.Producer;
 import com.example.isa.model.locator.TrackingRequestStatus;
 import com.example.isa.service.interfaces.TrackingRequestService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,15 +11,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Slf4j
 @Component
 public class TrackingResponseConsumer {
     private final ObjectMapper objectMapper;
     private final TrackingRequestService trackingRequestService;
+    private final Producer producer;
+    private boolean started = false;
 
-    public TrackingResponseConsumer(ObjectMapper objectMapper, TrackingRequestService trackingRequestService) {
+    public TrackingResponseConsumer(ObjectMapper objectMapper, TrackingRequestService trackingRequestService, Producer producer) {
         this.objectMapper = objectMapper;
         this.trackingRequestService = trackingRequestService;
+        this.producer = producer;
     }
 
     @KafkaListener(topics = "tracking.response.topic", groupId = "locator")
@@ -27,5 +32,12 @@ public class TrackingResponseConsumer {
         log.info("message consumed {}", message);
         TrackingResponseDto dto = objectMapper.readValue(message, TrackingResponseDto.class);
         trackingRequestService.updateStatus(dto.id(), TrackingRequestStatus.valueOf(dto.status()));
+        if(!started) {
+            producer.send(new BloodRequestResponseDto(dto.id(), "TO_BE_SENT"));
+            started = true;
+        }
+        if(Objects.equals(dto.status(), "COMPLETED")) {
+            producer.send(new BloodRequestResponseDto(dto.id(), "FULFILLED"));
+        }
     }
 }
